9fa38f0d6375103336053a4e02ecc36046e9da21
Pedro Monteiro <pedrogmonteiro02@gmail.com>
2025-04-29 14:11:18 +0100

    testing workflow

--- backend/auth-service/src/index.ts
+++ backend/auth-service/src/index.ts
@@ -32,7 +32,7 @@ type StartServer = () => Promise<void>;
 const start: StartServer = async () => {
   try {
     await app.listen({ port: Number(process.env.PORT) || 3000, host: '0.0.0.0' });
-    app.log.info(`Server running in http://localhost:${process.env.PORT || 3000}`);
+    app.log.info(`🚀 Server running in http://localhost:${process.env.PORT || 3000}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
